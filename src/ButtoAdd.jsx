62f9615d669aa54500e2104d87ad08250ea6c868
Luqman <luqmanrasa@gmail.com>
2024-12-14 12:18:22 +0800

    optimize sikit

--- src/ButtoAdd.jsx
+++ src/ButtoAdd.jsx
@@ -5,46 +5,46 @@ import myr20 from "./assets/myr20_small.png";
 import myr50 from "./assets/myr50_small.png";
 import myr100 from "./assets/myr100_small.png";
 
-export default function ButtonAdd({ type, onClick }) {
-  const types = {
-    1: {
-      text: "RM1",
-      bgColor: "bg-sky-800",
-      borderColor: "border-sky-900",
-      image: myr1,
-    },
-    5: {
-      text: "RM5",
-      bgColor: "bg-green-800",
-      borderColor: "border-green-900",
-      image: myr5,
-    },
-    10: {
-      text: "RM10",
-      bgColor: "bg-red-800",
-      borderColor: "border-red-900",
-      image: myr10,
-    },
-    20: {
-      text: "RM20",
-      bgColor: "bg-yellow-800",
-      borderColor: "border-yellow-900",
-      image: myr20,
-    },
-    50: {
-      text: "RM50",
-      bgColor: "bg-teal-800",
-      borderColor: "border-teal-900",
-      image: myr50,
-    },
-    100: {
-      text: "RM100",
-      bgColor: "bg-purple-800",
-      borderColor: "border-purple-900",
-      image: myr100,
-    },
-  };
+const types = {
+  1: {
+    text: "RM1",
+    bgColor: "bg-sky-800",
+    borderColor: "border-sky-900",
+    image: myr1,
+  },
+  5: {
+    text: "RM5",
+    bgColor: "bg-green-800",
+    borderColor: "border-green-900",
+    image: myr5,
+  },
+  10: {
+    text: "RM10",
+    bgColor: "bg-red-800",
+    borderColor: "border-red-900",
+    image: myr10,
+  },
+  20: {
+    text: "RM20",
+    bgColor: "bg-yellow-800",
+    borderColor: "border-yellow-900",
+    image: myr20,
+  },
+  50: {
+    text: "RM50",
+    bgColor: "bg-teal-800",
+    borderColor: "border-teal-900",
+    image: myr50,
+  },
+  100: {
+    text: "RM100",
+    bgColor: "bg-purple-800",
+    borderColor: "border-purple-900",
+    image: myr100,
+  },
+};
 
+export default function ButtonAdd({ type, onClick }) {
   const buttonType = types[type];
 
   if (!buttonType) return null;
